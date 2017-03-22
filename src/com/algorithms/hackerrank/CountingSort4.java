c8e8f2c6f5d9e383984de3cbe178d978c370c32b
Daniel Roldan Amaya <roldaniel89@gmail.com>
2017-03-22 17:41:53 -0500

    Create CountingSort4.java

---
+++ src/com/algorithms/hackerrank/CountingSort4.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class CountingSort4 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        int n = in.nextInt();
+        in.nextLine();
+        
+        StringBuilder[] arr = new StringBuilder[100];
+        for (int i = 0; i < n; i++) {
+            String[] values = in.nextLine().split(" ");
+            int x = Integer.parseInt(values[0]);
+            String s = values[1];
+            
+            if (i < (n / 2)) 
+                s = "-";
+            
+            StringBuilder sb = arr[x];
+            if (sb == null)  {
+                sb = new StringBuilder(n);
+                arr[x] = sb;
+            }
+            
+            sb.append(s);
+            sb.append(" ");
+        }
+        
+        for (int i = 0; i < arr.length; i++) {
+            StringBuilder sb = arr[i];
+            if (sb != null) {
+                System.out.print(sb.toString());
+            }
+        }
+    }
+}
