6a8a5f64d05caf20f64ff11a6f68bf5d2da5f01d
Daniel Roldan Amaya <roldaniel89@gmail.com>
2017-03-22 17:00:24 -0500

    Create CountingSort3.java

---
+++ src/com/algorithms/hackerrank/CountingSort3.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class CountingSort3 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] arr = new int[100];
+        
+        int n = in.nextInt();
+        in.nextLine();
+        
+        for (int i = 0; i < n; i++) {
+            int index = Integer.parseInt(in.nextLine().split(" ")[0]);
+            arr[index] += 1;
+        }
+        
+        int x = 0;
+        for (int i = 0; i < arr.length; i++) {
+            x += arr[i];
+            System.out.print(x + " ");
+        }
+    }
+}
