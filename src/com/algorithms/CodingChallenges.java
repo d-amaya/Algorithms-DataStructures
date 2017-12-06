598ed2f6489015067306b66b24404ad779f9a664
Daniel Roldan Amaya <roldaniel89@gmail.com>
2017-12-06 15:33:18 -0500

    Create CodingChallenges.java

---
+++ src/com/algorithms/CodingChallenges.java
@@ -0,0 +1,51 @@
+package com.algorithms;
+
+public class CodingChallenges {
+
+    /*
+    * Count repeated characters in a String
+    * Example:
+    *
+    * abcde   = 0
+    * abacada = 1
+    * abcbdee = 2
+    * abaaabb = 2
+    *
+    * When the String has less than two characters, you should return an Exception.
+    * */
+    public int countRepeatedCharacters(String s) {
+        return 0;
+    }
+
+
+    /*
+    * Verify whether the String is a palindrome or not.
+    * Example:
+    *
+    * ana   = true
+    * abba  = true
+    * abcba = true
+    * abaabb = false
+    *
+    * When the String has less than two characters, you should return an Exception.
+    * */
+    public boolean isPalindrome(String s) {
+        return false;
+    }
+
+
+    /*
+    * Verify whether the Parenthesis are balanced or not.
+    * Example:
+    *
+    * ()()()    = true
+    * ()((()))  = true
+    * ())(()()) = false
+    * )(())(    = false
+    *
+    * When the String has less than two characters, you should return an Exception.
+    * */
+    public boolean isBalanced(String s) {
+        return false;
+    }
+}
